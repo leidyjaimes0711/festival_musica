5790675705688b85abb1be2601465fc2b543e4b7
Leidy Jaimes <leidyjaimes0711@gmail.co>
2023-03-28 07:44:44 -0500

    version 2 del 28 de marzo

--- src/js/app.js
+++ src/js/app.js
@@ -8,6 +8,52 @@ function iniciarApp () {
 
 function crearGaleria () {
     const galeria = document.querySelector('.galeria-imagenes');
-    galeria.textContent = 'Vamos a crear la galería';
+
+    for (let i = 1; i <=12; i++) {
+        const imagen = document.createElement('picture');
+        imagen.innerHTML = `
+        <source srcset="build/img/thumb/${i}.avif" type="image/avif">
+        <source srcset="build/img/thumb/${i}.webp" type="image/webp">
+        <img loading="lazy" width="200" height="300" src="build/img/thumb/${i}.jpg" alt="imagen galeria">
+        `
+
+        imagen.onclick = function() {
+            mostrarImagen(i);
+        }
+        galeria.appendChild(imagen);
+        
+    }
+
+}
+
+function mostrarImagen (id) {
+    const imagen = document.createElement('picture');
+    imagen.innerHTML = `
+    <source srcset="build/img/grande/${id}.avif" type="image/avif">
+    <source srcset="build/img/grande/${id}.webp" type="image/webp">
+    <img loading="lazy" width="200" height="300" src="build/img/grande/${id}.jpg" alt="imagen galeria">
+    `;
+    //crea el overlay con la imagen
+    const overlay = document.createElement('DIV');
+    overlay.appendChild(imagen);
+    overlay.classList.add('overlay');
+
+
+    //boton para cerrar el modal (galeria)
+    const cerrarModal = document.createElement('P');
+    cerrarModal.textContent = 'X';
+    cerrarModal.classList.add('boton-cerrar');
+    cerrarModal.onclick = function() {
+        const body = document.querySelector('body');
+        body.classList.remove('fijar-body');
+        overlay.remove();
+    }
+    overlay.appendChild(cerrarModal);
+
+    //añade el overlay al html
+    const body = document.querySelector('body');
+    body.appendChild(overlay);
+    body.classList.add('fijar-body');
+
 
 }
